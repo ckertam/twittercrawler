c8e1ffd22c0cc82566632942007b6771c3168381
ilaydakurtboun <ilayda.kurt@boun.edu.tr>
2022-12-20 00:48:16 +0300

    added mock data

--- app/tweet/views.py
+++ app/tweet/views.py
@@ -49,7 +49,7 @@ class GetTweetsSimulationAPIView(APIView):
         username=request.data.get("username")
         since_date = request.data.get("since_date")
         data = request.data.get("data")
-        file_path = os.path.abspath("app/tweet/TestExcel.xlsx")
+        file_path = os.path.abspath("tweet/TestExcel.xlsx")
         df = pd.read_excel(file_path)
         # df = pd.read_excel("/TestExcel.xlsx")
         # tweet = df["tweet"].values.tolist()
